68a8786194b1e3e5245e275bcc0faf8dcf7745f7
juliausanova <juliausanova@gmail.com>
2019-11-22 18:42:59 +0300

    init commit

---
+++ nuxt.config.js
@@ -0,0 +1,117 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'public-web',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  modules: [
+    [
+      'nuxt-i18n',
+      {
+        locales: ['en', 'fr', 'es'],
+        ueI18nLoader: false,
+        // Separator used to generated routes name for each locale, you shouldn't
+        // need to change this
+        routesNameSeparator: '___',
+
+        // Suffix added to generated routes name for default locale if strategy is prefix_and_default,
+        // you shouldn't need to change this
+        defaultLocaleRouteNameSuffix: 'default',
+
+        // Routes generation strategy, can be set to one of the following:
+        // - 'no_prefix': routes won't be prefixed
+        // - 'prefix_except_default': add locale prefix for every locale except default
+        // - 'prefix': add locale prefix for every locale
+        // - 'prefix_and_default': add locale prefix for every locale and default
+        strategy: 'prefix_except_default',
+        // Wether or not the translations should be lazy-loaded, if this is enabled,
+        // you MUST configure langDir option, and locales must be an array of objects,
+        // each containing a file key
+        lazy: false,
+
+        // Directory that contains translations files when lazy-loading messages,
+        // this CAN NOT be empty if lazy-loading is enabled
+        langDir: null,
+
+        // Set this to a path to which you want to redirect users accessing root URL (/)
+        // rootRedirect: 'index.html',
+        // Enable browser language detection to automatically redirect user
+        // to their preferred language as they visit your app for the first time
+        // Set to false to disable
+        detectBrowserLanguage: {
+          // If enabled, a cookie is set once a user has been redirected to his
+          // preferred language to prevent subsequent redirections
+          // Set to false to redirect every time
+          useCookie: true,
+          // Cookie name
+          cookieKey: 'i18n_redirected',
+          // Set to always redirect to value stored in the cookie, not just once
+          alwaysRedirect: false,
+          // If no locale for the browsers locale is a match, use this one as a fallback
+          fallbackLocale: null
+        },
+        // If true, SEO metadata is generated for routes that have i18n enabled.
+        // Note that performance can suffer with this enabled and there might be compatibility
+        // issues with some plugins. Recommended way is to set up SEO as described in:
+        // https://nuxt-community.github.io/nuxt-i18n/seo.html#improving-performance
+        seo: false,
+        // Fallback base URL to use as prefix for alternate URLs in hreflang tags.
+        // By default VueRouter's base URL will be used and only if that is not available,
+        // fallback URL will be used.
+        baseUrl: '',
+        defaultLocale: 'en',
+        vueI18n: {
+          fallbackLocale: 'en',
+          messages: {
+            en: {
+              welcome: 'Welcome'
+            },
+            fr: {
+              welcome: 'Bienvenue'
+            },
+            es: {
+              welcome: 'Bienvenido'
+            }
+          }
+        },
+        // Called right before app's locale changes
+        beforeLanguageSwitch: (oldLocale, newLocale) => null,
+
+        // Called after app's locale has changed
+        onLanguageSwitched: (oldLocale, newLocale) => null
+      }
+    ]
+  ],
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
---
+++ pages/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+
+      <h2 class="subtitle">
+        Home page
+      </h2>
+
+    <p class="text-bold">
+      Lorem ipsum dolor sit amet, an meis lorem choro cum, mel mundi causae eu. Dicant eripuit nec at,
+      in invenire postulant has. Partiendo repudiare adolescens eu sit. Meis detraxit disputationi duo eu, fabellas verterem phaedrum has an, inimicus appellantur vis ne. Nam laoreet civibus ut. Vis vitae iudico constituto te, ex eos aeterno gloriatur.
+    </p>
+
+    <p class="text-muted">
+      Sit ad simul gloriatur, ea ignota lobortis ius. Ut legimus tibique scaevola mei, vis omnis ludus corpora et.
+      Veri perfecto definitiones nam ea. Cum et omnium molestie, per omnes fierent ut.
+    </p>
+  </section>
+</template>
+
+<script>
+  export default {}
+</script>
